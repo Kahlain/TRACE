6be526a27afd698428e99ba7f4b04e1eddfcddab
Kahlain <mo@kahlain.co>
2024-10-29 12:58:09 -0400

    rollback

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import FrameworkWheel from './components/FrameworkWheel';
-import DetailPanel from './components/DetailPanel';
-// ... rest of the file remains the same 
